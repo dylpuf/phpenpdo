fd05224148646617f03da80a130eb943540023ad
dylpuf <131676226+dylpuf@users.noreply.github.com>
2023-05-11 14:45:18 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+
+<pre>
+<?php
+$eengetal = 10;
+$tweegetal = 12;
+
+$antwoord = $eengetal + $tweegetal;
+
+echo $eengetal . "+" . $tweegetal . "=" . $antwoord. "<br>";
+
+$o = 10;
+$r = 12;
+
+$antwoord2 = $o + $r;
+
+echo $o . "*" . $r . "=" . $antwoord2."<br>";
+
+$naamMijn = " Dylan";
+
+echo "Welkom".$naamMijn;
+
+?>
+</pre>
+
+</body>
+
+</html>
