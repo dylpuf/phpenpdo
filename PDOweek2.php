ba627ffb33bc5945f9a9a49e461ea1e7a0c14449
dylpuf <131676226+dylpuf@users.noreply.github.com>
2023-05-25 15:54:27 +0200

    Update PDOweek2.php

--- PDOweek2.php
+++ PDOweek2.php
@@ -9,7 +9,7 @@
     $password = '';
     $charset = 'utf8mb4_unicode_ci';
 
-    $dsn = "localhost:3307=$host;winkel=$db;utf8mb4_unicode_ci=$charset";
+    $dsn = "localhost:3307=$host;winkel=$db;utf8mb4=$charset";
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
