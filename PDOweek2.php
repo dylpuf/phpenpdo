5a6034508df0db09fa22fbb1637a6fcee3a07b52
dylpuf <131676226+dylpuf@users.noreply.github.com>
2023-05-25 14:29:44 +0200

    Add files via upload

---
+++ PDOweek2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<pre>
+    <?php
+    $host = 'localhost:3307';
+    $db = 'winkel';
+    $user = 'root';
+    $password = '';
+    $charset = 'utf8mb4_unicode_ci';
+
+    $dsn = "localhost:3307=$host;winkel=$db;utf8mb4_unicode_ci=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    try
+    {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+        echo "Connectie gemaakt!";
+   } 
+   catch (\PDOException $e) 
+   {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+    ?>
+</pre>
+</head>
+<body>
+    
+</body>
+</html>
